4c303ee8969160548df295a9183ee32855f8ed3a
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-17 13:29:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(act = "roller-skate") {
+  return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(act = "go to the office") {
+  return `This Monday, I will ${act}.`;
+}
+
+function wrapAdjective(wrap) {
+  return function (adj) {
+    return `You are ${wrap}${adj}${wrap}!`;
+  };
+}
